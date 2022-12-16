598df15b1506a54c2fabef4e2205bd58627c9ed6
diptosarkar <diptosarkar@users.noreply.github.com>
2022-12-16 09:19:10 -0500

    Added user routes

--- attendance-tracker/routes/users.js
+++ attendance-tracker/routes/users.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET cool listing. */
+router.get('/cool', function(req, res, next) {
+  res.send('You are so cool!');
+});
+
+/* GET uncool listing. */
+router.get('/uncool', function(req, res, next) {
+  res.send('Why be unchill. Be hot!');
+});
+
+
 module.exports = router;
